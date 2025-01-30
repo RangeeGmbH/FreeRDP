cc396fa301b53eff436ce1338c08318129c182ef
René Vögeli <rvoegeli@rangee.com>
2025-01-30 12:56:21 +0100

    Add command line flag to disable Ctrl+Alt+M hotkey to minimize application

--- libfreerdp/common/settings_getters.c
+++ libfreerdp/common/settings_getters.c
@@ -371,6 +371,9 @@ BOOL freerdp_settings_get_bool(WINPR_ATTR_UNUSED const rdpSettings* settings,
 		case FreeRDP_MaximizeShell:
 			return settings->MaximizeShell;
 
+		case FreeRDP_MinimizeHotkey:
+			return settings->MinimizeHotkey;
+
 		case FreeRDP_MouseAttached:
 			return settings->MouseAttached;
 
@@ -1087,6 +1090,10 @@ BOOL freerdp_settings_set_bool(WINPR_ATTR_UNUSED rdpSettings* settings,
 
 		case FreeRDP_MaximizeShell:
 			settings->MaximizeShell = cnv.c;
+			break;
+
+		case FreeRDP_MinimizeHotkey:
+			settings->MinimizeHotkey = cnv.c;
 			break;
 
 		case FreeRDP_MouseAttached:
