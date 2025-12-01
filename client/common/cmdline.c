973e30fa546dad219f1228041868bddc219d3a4f
Jens Ahlschläger <jahlschlaeger@rangee.com>
2025-12-01 16:50:20 +0100

    Add command line flag to disable Ctrl+Alt+D hotkey to end session

--- client/common/cmdline.c
+++ client/common/cmdline.c
@@ -5303,6 +5303,11 @@ static int parse_command_line(rdpSettings* settings, const COMMAND_LINE_ARGUMENT
 			if (!freerdp_settings_set_bool(settings, FreeRDP_MinimizeHotkey, enable))
 				return fail_at(arg, COMMAND_LINE_ERROR);
 		}
+		CommandLineSwitchCase(arg, "endsession-hotkey")
+		{
+			if (!freerdp_settings_set_bool(settings, FreeRDP_EndSessionHotkey, enable))
+				return fail_at(arg, COMMAND_LINE_ERROR);
+		}
 		CommandLineSwitchCase(arg, "force-console-callbacks")
 		{
 			if (!freerdp_settings_set_bool(settings, FreeRDP_UseCommonStdioCallbacks, enable))
